35828bbb18f94fa7b2183c95d44a24be4bc387c9
Aashu24 <anejaaastha@gmail.com>
2017-04-03 23:38:57 +0530

    Create aVeryBigSum.java

---
+++ Algorithms/Warmup/aVeryBigSum.java
@@ -0,0 +1,25 @@
+//Hackerrank Solutions
+//Algorithms
+//Warmup
+//A Very Big Sum
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        long[] arr=new long[n];
+        long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=in.nextInt();
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+    }
+}
