a330d6dddaf1f287861d5fda67351af475f5de5d
Aashu24 <anejaaastha@gmail.com>
2017-04-03 23:35:33 +0530

    Create compareTheTriplets.java

---
+++ Algorithms/Warmup/compareTheTriplets.java
@@ -0,0 +1,33 @@
+//Hackerrank Solutions
+//Algorithms
+//Warmup
+//Compare The Triplets
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] a=new int[3];
+        int[] b=new int[3];
+        int ascore=0,bscore=0,i;
+        for(i=0;i<3;i++)
+            a[i]=in.nextInt();
+        for(i=0;i<3;i++)
+            b[i]=in.nextInt();
+        for(i=0;i<3;i++)
+        {
+            if(a[i]>b[i])
+                ascore++;
+            else if(b[i]>a[i])
+                bscore++;
+        }   
+        System.out.print(ascore+" "+bscore);
+    }
+}
