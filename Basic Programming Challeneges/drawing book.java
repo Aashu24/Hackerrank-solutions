8acd6e697a5fc1b82536b6c6dddb744b818b848d
Aashu24 <anejaaastha@gmail.com>
2017-04-05 20:53:58 +0530

    Create drawing book.java

---
+++ Basic Programming Challeneges/drawing book.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int p = in.nextInt();
+    System.out.println(Math.min(p/2,(n-p)/2));
+}
+}
