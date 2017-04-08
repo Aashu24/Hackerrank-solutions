9173c70dac2d4fb210a6bba6b4433698a8b28f7c
Aashu24 <anejaaastha@gmail.com>
2017-04-08 21:43:52 +0530

    Create Cats and a Mouse.java

---
+++ Basic Programming Challeneges/Cats and a Mouse.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            int x = in.nextInt();
+            int y = in.nextInt();
+            int z = in.nextInt();
+            int a = Math.abs(x-z);
+            int b = Math.abs(y-z);
+            if(a<b)
+                System.out.println("Cat A");
+            else if(b<a)
+                System.out.println("Cat B");
+            else
+                System.out.println("Mouse C");
+        }
+    }
+}
